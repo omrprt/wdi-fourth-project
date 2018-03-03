5b9a2277788114346c9d77d764894426904983a2
Omar Portillo <OmrPrt@aol.com>
2018-03-03 11:15:33 +0000

    fixed camelcase on familyand friends

--- src/components/users/UsersNetwork.js
+++ src/components/users/UsersNetwork.js
@@ -3,7 +3,7 @@ import Axios from 'axios';
 import Auth from '../../lib/Auth';
 import { PanelGroup, Panel } from 'react-bootstrap';
 
-import UsersNewFamilyandFriends from './UsersNewFamilyandFriends';
+import UsersNewFamilyandFriends from './UsersNewFamilyAndFriends';
 import UsersNewProfessional from './UsersNewProfessional';
 import UsersNewOrganization from './UsersNewOrganization';
 

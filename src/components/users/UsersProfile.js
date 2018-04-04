9457c105bc391ddaeb7aa4859511513aaa7cae5a
Omar Portillo <OmrPrt@aol.com>
2018-04-04 11:36:05 +0100

    sylted plans display on the wide screen

--- src/components/users/UsersProfile.js
+++ src/components/users/UsersProfile.js
@@ -65,34 +65,34 @@ class UsersProfile extends Component {
               <Panel.Toggle componentClass="a"><i className="fa fa-caret-down fa-3x"></i></Panel.Toggle>
               <Panel.Title>My Support Network</Panel.Title>
               <Link to={`/users/${this.state.user.id}/mynetwork`}><i className="fa fa-user-plus fa-2x"></i></Link>
-          </Panel.Heading>
-          <Panel.Collapse>
-            <Panel.Body>
+            </Panel.Heading>
+            <Panel.Collapse>
+              <Panel.Body>
 
-              <Grid>
-                <Panel className="contacts">
-                  <Panel.Heading  ><h3>My Friends and Family Support</h3></Panel.Heading>
-                  <ListGroup>
-                    {this.state.user.myFamilyandFriends && this.state.user.myFamilyandFriends.map((myFamilyandFriend, index) =>
-                      <div key={index}>
-                        <ListGroupItem>
-                          <Row className="show-grid">
-                            <Col xs={4} md={4}>
-                              {myFamilyandFriend.name}
-                            </Col>
-                            <Col xs={4} md={4}>
-                              {myFamilyandFriend.relationship}
-                            </Col>
-                            <Col xs={4} md={4}>
-                              <a href={`tel:${myFamilyandFriend.phoneNumber}`}><i className="fa fa-phone-square"></i> {myFamilyandFriend.phoneNumber}</a>
-                            </Col>
-                          </Row>
-                        </ListGroupItem>
-                      </div>
-                    )}
-                  </ListGroup>
-                </Panel>
-              </Grid>
+                <Grid>
+                  <Panel className="contacts">
+                    <Panel.Heading  ><h3>My Friends and Family Support</h3></Panel.Heading>
+                    <ListGroup>
+                      {this.state.user.myFamilyandFriends && this.state.user.myFamilyandFriends.map((myFamilyandFriend, index) =>
+                        <div key={index}>
+                          <ListGroupItem>
+                            <Row className="show-grid">
+                              <Col xs={4} md={4}>
+                                {myFamilyandFriend.name}
+                              </Col>
+                              <Col xs={4} md={4}>
+                                {myFamilyandFriend.relationship}
+                              </Col>
+                              <Col xs={4} md={4}>
+                                <a href={`tel:${myFamilyandFriend.phoneNumber}`}><i className="fa fa-phone-square"></i> {myFamilyandFriend.phoneNumber}</a>
+                              </Col>
+                            </Row>
+                          </ListGroupItem>
+                        </div>
+                      )}
+                    </ListGroup>
+                  </Panel>
+                </Grid>
 
                 <Grid>
                   <Panel className="contacts">
@@ -119,19 +119,110 @@ class UsersProfile extends Component {
                   </Panel>
                 </Grid>
 
+                <Grid>
+                  <Panel className="contacts">
+                    <Panel.Heading ><h3>Organizations</h3></Panel.Heading>
+                    <ListGroup>
+                      {this.state.user.myOrganizations && this.state.user.myOrganizations.map((myOrganizations, index) =>
+                        <div key={index}>
+                          <ListGroupItem>
+                            <Row className="show-grid">
+                              <Col xs={8} md={8}>
+                                <a href={ myOrganizations.url}><i className="fa fa-globe"></i> {myOrganizations.name}</a>
+                              </Col>
+                              <Col xs={4} md={4}>
+                                <a href={`tel:${myOrganizations.phoneNumber}`}><i className="fa fa-phone-square"></i> {myOrganizations.phoneNumber}</a>
+                              </Col>
+                            </Row>
+                          </ListGroupItem>
+                        </div>
+                      )}
+                    </ListGroup>
+                  </Panel>
+                </Grid>
+
+              </Panel.Body>
+            </Panel.Collapse>
+          </Panel>
+
+          <Panel>
+            <Panel.Heading>
+              <Panel.Toggle componentClass="a"><i className="fa fa-caret-down fa-3x"></i></Panel.Toggle>
+              <Panel.Title>My Plans</Panel.Title>
+              <Link to={'/myplans'}>  <i className="fa fa-book fa-2x"></i></Link>
+            </Panel.Heading>
+            <Panel.Collapse>
+              <Panel.Body>
+
+                <Grid>
+                  {this.state.user.plans && this.state.user.plans.map((plan, index) =>
+                    <div key={index}>
+                      <Link to={`/myplans/crisisplan/${plan._id}`}>
+                        <ListGroup>
+                          <ListGroupItem className="planNames">
+                            <Row >
+                              { plan.title }
+                            </Row>
+
+                          </ListGroupItem>
+                        </ListGroup>
+                      </Link>
+                    </div>
+
+                  )}
+
+                </Grid>
+              </Panel.Body>
+            </Panel.Collapse>
+          </Panel>
+        </div>
+
+        <div className="xFull">
+          <Tabs defaultActiveKey={1} animation={false} id="fullTabs">
+            <Tab eventKey={1} title="Thought Diary">
+
+              <Grid>
+                {this.state.user.diaries && this.state.user.diaries.map((diary, index) =>
+                  <div key={index}>
+
+                    <Link to={`/diaries/${diary._id}`}>
+                      <ListGroup>
+                        <ListGroupItem className="dates">
+                          <Row >
+                            <Col lg={6}>
+                              { (new Date(diary.createdAt)).toLocaleDateString('en-UK', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' })}
+                            </Col>
+                            <Col lg={6}>
+                              {diary.title}
+                            </Col>
+                          </Row>
+
+                        </ListGroupItem>
+                      </ListGroup>
+
+                    </Link>
+                  </div>
+                )}
+              </Grid>
+            </Tab>
+            <Tab eventKey={2} title="My Support Network">
+
               <Grid>
                 <Panel className="contacts">
-                  <Panel.Heading ><h3>Organizations</h3></Panel.Heading>
+                  <Panel.Heading  ><h3>My Friends and Family Support</h3></Panel.Heading>
                   <ListGroup>
-                    {this.state.user.myOrganizations && this.state.user.myOrganizations.map((myOrganizations, index) =>
+                    {this.state.user.myFamilyandFriends && this.state.user.myFamilyandFriends.map((myFamilyandFriend, index) =>
                       <div key={index}>
                         <ListGroupItem>
                           <Row className="show-grid">
-                            <Col xs={8} md={8}>
-                              <a href={ myOrganizations.url}><i className="fa fa-globe"></i> {myOrganizations.name}</a>
+                            <Col xs={4} md={4}>
+                              {myFamilyandFriend.name}
                             </Col>
                             <Col xs={4} md={4}>
-                              <a href={`tel:${myOrganizations.phoneNumber}`}><i className="fa fa-phone-square"></i> {myOrganizations.phoneNumber}</a>
+                              {myFamilyandFriend.relationship}
+                            </Col>
+                            <Col xs={4} md={4}>
+                              <a href={`tel:${myFamilyandFriend.phoneNumber}`}><i className="fa fa-phone-square"></i> {myFamilyandFriend.phoneNumber}</a>
                             </Col>
                           </Row>
                         </ListGroupItem>
@@ -141,170 +232,73 @@ class UsersProfile extends Component {
                 </Panel>
               </Grid>
 
-            </Panel.Body>
-          </Panel.Collapse>
-        </Panel>
+              <Grid>
+                <Panel className="contacts">
+                  <Panel.Heading ><h3>My Professional Support</h3></Panel.Heading>
+                  <ListGroup>
+                    {this.state.user.myProfessionals && this.state.user.myProfessionals.map((myProfessional, index) =>
+                      <div key={index}>
+                        <ListGroupItem>
+                          <Row className="show-grid">
+                            <Col xs={4} md={4}>
+                              {myProfessional.name}
+                            </Col>
+                            <Col xs={4} md={4}>
+                              {myProfessional.profession}
+                            </Col>
+                            <Col xs={4} md={4}>
+                              <a href={`tel:${myProfessional.phoneNumber}`}><i className="fa fa-phone-square"></i> {myProfessional.phoneNumber}</a>
+                            </Col>
+                          </Row>
+                        </ListGroupItem>
+                      </div>
+                    )}
+                  </ListGroup>
+                </Panel>
+              </Grid>
 
-        <Panel>
-          <Panel.Heading>
-            <Panel.Toggle componentClass="a"><i className="fa fa-caret-down fa-3x"></i></Panel.Toggle>
-            <Panel.Title>My Plans</Panel.Title>
-            <Link to={'/myplans'}>  <i className="fa fa-book fa-2x"></i></Link>
-          </Panel.Heading>
-          <Panel.Collapse>
-            <Panel.Body>
+              <Grid>
+                <Panel className="contacts">
+                  <Panel.Heading ><h3>Organizations</h3></Panel.Heading>
+                  <ListGroup>
+                    {this.state.user.myOrganizations && this.state.user.myOrganizations.map((myOrganizations, index) =>
+                      <div key={index}>
+                        <ListGroupItem>
+                          <Row className="show-grid">
+                            <Col xs={8} md={8}>
+                              <a href={ myOrganizations.url}><i className="fa fa-globe"></i> {myOrganizations.name}
+                              </a>
+                            </Col>
+                            <Col xs={4} md={4}>
+                              <a href={`tel:${myOrganizations.phoneNumber}`}><i className="fa fa-phone-square"></i> {myOrganizations.phoneNumber}</a>
+                            </Col>
 
+                          </Row>
+                        </ListGroupItem>
+                      </div>
+                    )}
+                  </ListGroup>
+                </Panel>
+              </Grid>
+            </Tab>
+            <Tab eventKey={3} title="My Plans">
               <Grid>
                 {this.state.user.plans && this.state.user.plans.map((plan, index) =>
                   <div key={index}>
                     <Link to={`/myplans/crisisplan/${plan._id}`}>
-                    <ListGroup>
-                      <ListGroupItem className="planNames">
-                        <Row >
-                            { plan.title }
-                        </Row>
-
-                      </ListGroupItem>
-                    </ListGroup>
-                  </Link>
-                </div>
-
-              )}
-
-            </Grid>
-            </Panel.Body>
-          </Panel.Collapse>
-        </Panel>
-      </div>
-
-      <div className="xFull">
-        <Tabs defaultActiveKey={1} animation={false} id="fullTabs">
-          <Tab eventKey={1} title="Thought Diary">
-            {/* <ul>
-              {this.state.user.diaries && this.state.user.diaries.map((diary, index) =>
-                <li key={index}>
-                  <Link className="dates" to={`/diaries/${diary._id}`}>{diary.title}</Link>
-                </li>
-              )}
-            </ul> */}
-            <Grid>
-              {this.state.user.diaries && this.state.user.diaries.map((diary, index) =>
-                <div key={index}>
-
-                  <Link to={`/diaries/${diary._id}`}>
-                    <ListGroup>
-                      <ListGroupItem className="dates">
-                        <Row >
-                          <Col lg={6}>
-                            { (new Date(diary.createdAt)).toLocaleDateString('en-UK', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' })}
-                          </Col>
-                          <Col lg={6}>
-                            {diary.title}
-                          </Col>
-                        </Row>
-
-                      </ListGroupItem>
-                    </ListGroup>
-
-                  </Link>
-                </div>
-              )}
-            </Grid>
-          </Tab>
-          <Tab eventKey={2} title="My Support Network">
-
-            <Grid>
-              <Panel className="contacts">
-                <Panel.Heading  ><h3>My Friends and Family Support</h3></Panel.Heading>
-                <ListGroup>
-                  {this.state.user.myFamilyandFriends && this.state.user.myFamilyandFriends.map((myFamilyandFriend, index) =>
-                    <div key={index}>
-                      <ListGroupItem>
-                        <Row className="show-grid">
-                          <Col xs={4} md={4}>
-                            {myFamilyandFriend.name}
-                          </Col>
-                          <Col xs={4} md={4}>
-                            {myFamilyandFriend.relationship}
-                          </Col>
-                          <Col xs={4} md={4}>
-                            <a href={`tel:${myFamilyandFriend.phoneNumber}`}><i className="fa fa-phone-square"></i> {myFamilyandFriend.phoneNumber}</a>
-                          </Col>
-                        </Row>
-                      </ListGroupItem>
-                    </div>
-                  )}
-                </ListGroup>
-              </Panel>
-            </Grid>
-
-            <Grid>
-              <Panel className="contacts">
-                <Panel.Heading ><h3>My Professional Support</h3></Panel.Heading>
-                <ListGroup>
-                  {this.state.user.myProfessionals && this.state.user.myProfessionals.map((myProfessional, index) =>
-                    <div key={index}>
-                      <ListGroupItem>
-                        <Row className="show-grid">
-                          <Col xs={4} md={4}>
-                            {myProfessional.name}
-                          </Col>
-                          <Col xs={4} md={4}>
-                            {myProfessional.profession}
-                          </Col>
-                            <Col xs={4} md={4}>
-                            <a href={`tel:${myProfessional.phoneNumber}`}><i className="fa fa-phone-square"></i> {myProfessional.phoneNumber}</a>
-                          </Col>
-                        </Row>
-                      </ListGroupItem>
-                    </div>
-                  )}
-                </ListGroup>
-              </Panel>
-            </Grid>
-
-            <Grid>
-              <Panel className="contacts">
-                <Panel.Heading ><h3>Organizations</h3></Panel.Heading>
-                <ListGroup>
-                  {this.state.user.myOrganizations && this.state.user.myOrganizations.map((myOrganizations, index) =>
-                    <div key={index}>
-                      <ListGroupItem>
-                        <Row className="show-grid">
-                          <Col xs={8} md={8}>
-                            <a href={ myOrganizations.url}><i className="fa fa-globe"></i> {myOrganizations.name}
-                          </a>
-                        </Col>
-                        <Col xs={4} md={4}>
-                          <a href={`tel:${myOrganizations.phoneNumber}`}><i className="fa fa-phone-square"></i> {myOrganizations.phoneNumber}</a>
-                        </Col>
-
-                      </Row>
-                    </ListGroupItem>
-                  </div>
-                )}
-              </ListGroup>
-            </Panel>
-          </Grid>
-        </Tab>
-        <Tab eventKey={3} title="My Plans">
-          <Grid>
-            {this.state.user.strategies && this.state.user.strategies.map((strategy, index) =>
-                  <div key={index}>
-
-                    <Link to={`/diaries/${strategy._id}`}>
                       <ListGroup>
-                        <ListGroupItem className="dates">
+                        <ListGroupItem className="planNames">
                           <Row >
-                            { strategy }
+                            { plan.title }
                           </Row>
 
                         </ListGroupItem>
                       </ListGroup>
-
                     </Link>
                   </div>
+
                 )}
+
               </Grid>
             </Tab>
           </Tabs>

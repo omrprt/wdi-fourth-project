085ae034e5283ef4e071faeca52cf2b5384b2015
Omar Portillo <OmrPrt@aol.com>
2018-05-14 22:47:18 +0100

    added web testing disclaimer

--- src/components/utility/Index.js
+++ src/components/utility/Index.js
@@ -9,8 +9,10 @@ class Index extends Component {
       <div>
         <h2>
         Welcome to My Mental Health Planner, this website is a tool, in addition to your support network to manage your mental health.
+
+        This started as a final project for a web development immersive course with General Assembly, and it is only meant for testing, with the hope of developing into a comprehensive web application.
           <div className="emergency">
-        In case of an emergency go to your nearest hospital.
+        In case of an emergency call 999 or go to your nearest hospital.
           </div>
         </h2>
       </div>
